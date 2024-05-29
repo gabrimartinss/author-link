0edc4229f7d02543eb518e83ff26f4db2107446c
gabrimartinss <gabriel.martinsdesouza2@gmail.com>
2024-05-29 00:17:26 -0300

    feat: bootstrap devrake

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
+import "bootstrap"
 import "controllers"
+
+document.addEventListener("DOMContentLoaded", function() {
+  console.log("DOM fully loaded and parsed");
+});
