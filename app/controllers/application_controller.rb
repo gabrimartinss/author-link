36f55908d6d45464b886ef0b5c2f935a6593b939
gabrimartinss <gabriel.martinsdesouza2@gmail.com>
2024-06-05 00:16:13 -0300

    refactor docker and final updates

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
+
+  def set_locale
+    if params[:locale]
+      cookies[:locale] = params[:locale]
+    end
+
+    if cookies[:locale]
+      if I18n.locale != cookies[:locale].to_sym
+        I18n.locale = cookies[:locale].to_sym
+      end
+    end 
+  end
 end
